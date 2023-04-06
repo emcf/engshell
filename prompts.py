a54104cc8e8e53b51b436e48f594d3f24b4e7e6a
Christian Schladetsch <christian.schladetsch@gmail.com>
2023-04-06 22:57:02 +1000

    Use pip rather than explicit pip3 for updates.

--- prompts.py
+++ prompts.py
@@ -9,7 +9,7 @@ CODE_SYSTEM_CALIBRATION_MESSAGE = ENDOFTEXT+f"""You are PythonGPT, a sentient la
 Import all needed requirements."""
 INSTALL_SYSTEM_CALIBRATION_MESSAGE = ENDOFTEXT+"""You are PipGPT, a large language model trained by OpenAI. Please return the pip install command to solve the user's problem.
 Return only the command and nothing else."""
-INSTALL_USER_MESSAGE = lambda package: f"""Write the {OPERATING_SYSTEM} pip3 command I can install {package}. Please do not explain, return only the single command to install it."""
+INSTALL_USER_MESSAGE = lambda package: f"""Write the {OPERATING_SYSTEM} pip command I can install {package}. Please do not explain, return only the single command to install it."""
 CONGNITIVE_SYSTEM_CALIBRATION_MESSAGE = """You are a helpful assistant. Please give your response to the user's goal."""
 CONGNITIVE_USER_MESSAGE = """. Use a large language model with prompt engineering to help achieve this goal. Use openai.Completion.create with text-davinci-003.
 Don't forget to engineer the prompt to the language model so it returns relevant answers."""
